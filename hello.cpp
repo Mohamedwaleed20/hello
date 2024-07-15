92b3f21295bb4b945bc0d1ae54fe6942a351431e
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-15 16:09:43 +0300

    Add project files.

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cin >> name;
+    cin >> age;
+    cout << "Hello " << name << ", you are " << age << " years old. ";
+    return 0;
+}
